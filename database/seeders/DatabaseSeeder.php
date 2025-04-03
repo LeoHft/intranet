7dbe057d373cc20cb1066cbc35a7bc8965472105
LeoHft <leo.hofstetter@orange.fr>
2025-04-03 11:58:37 +0200

    Put all seeder logic in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,54 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('password'),
             'is_admin' => true,
         ]);
+
+
+        DB::table('categories')->insert([
+            [
+                'name' => 'Multimédia',
+                'description' => "Tout ce qui est multimédia",
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+
+        DB::table('status')->insert([
+            'name' => 'UP',
+            'description' => 'Le service est accessible',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('services')->insert([
+            [
+            'name' => 'test service',
+            'description' => 'test service description',
+            'internal_url' => 'http://localhost:8000',
+            'external_url' => 'https://testservice.com',
+            'image_url' => 'http://127.0.0.1:8000/storage/images/no-photo-available.jpg',
+            'status_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+            ]
+        ]);
+
+        DB::table('services_access')->insert([
+            [
+                'user_id' => 1,
+                'service_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+
+        DB::table('categories_services')->insert([
+            [
+                'service_id' => 1,
+                'category_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+
     }
 }
