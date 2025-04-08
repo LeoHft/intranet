0b8e3ecfa250c780f82bbacf2b540b878d988ad0
LeoHft <leo.hofstetter@orange.fr>
2025-04-08 21:25:47 +0200

    improve border style when focus

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -13,7 +13,7 @@ export default function NavLink({
                 'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
                 (active
                     ? 'border-rose-300 text-gray focus:border-rose-400'
-                    : 'border-transparent text-gray-500 hover:border-rose-300 hover:text-gray-700 focus:border-gray-300 focus:text-gray-700') +
+                    : 'border-transparent text-gray-500 hover:border-rose-300 hover:text-gray-700 focus:border-rose-400 focus:text-gray-700') +
                 className
             }
         >
