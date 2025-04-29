53e1c449f931f573be213d86725a2fa0d5c01112
LeoHft <leo.hofstetter@orange.fr>
2025-04-29 23:33:53 +0200

    test9

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,9 @@
 import axios from 'axios';
+
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//IMPORTANT** pour les requ  tes JSON :  
+// - envoie toujours les cookies  
+// - r  cup  re automatiquement le XSRF-cookie et le met dans le header X-XSRF-TOKEN
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.withXSRFToken = true;
