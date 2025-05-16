9a0ff6463d21997f799543bb429ea8634fdf8699
LeoHft <leo.hofstetter@orange.fr>
2025-05-16 18:41:37 +0200

    fix(delete middleware)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,6 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->api(prepend: [
             \Illuminate\Cookie\Middleware\EncryptCookies::class, // crypte/décrypte les cookies
-            \Illuminate\Session\Middleware\StartSession::class,  //démarre la session pour la requête en cours
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, //distinguer les requêtes « stateful » (celles qui proviennent d’un navigateur ou d’une SPA) des autres requêtes
         ]);
     })
