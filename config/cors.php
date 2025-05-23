6516d0d275de8494ec26effe2ce91657da2558d9
LeoHft <leo.hofstetter@orange.fr>
2025-05-23 17:38:27 +0200

    small changes to use on the server

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8080', 'http://localhost:8000', 'http://localhost:5173', 'http://127.0.0.1:8000', 'http://127.0.0.1:5173'], //domaines autorisés pour les requêtes CORS
+    'allowed_origins' => ['http://localhost:8080', 'http://localhost:8000', 'http://localhost:5173', 'http://127.0.0.1:8000', 'http://127.0.0.1:5173', 'https://intranet.hofstetterlab.ovh'], //domaines autorisés pour les requêtes CORS
 
     'allowed_origins_patterns' => [],
 
